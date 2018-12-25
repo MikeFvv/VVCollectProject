d1d5d3f88e98acdf8b11c855ba49f8df60de7f02
mike <mikefoog@gmail.com>
2018-12-25 22:59:09 +0800

    初始化项目

---
+++ VVCollectProject/VVCollectProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  VVCollectProject
+//
+//  Created by fangyuan on 2018/12/25.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
