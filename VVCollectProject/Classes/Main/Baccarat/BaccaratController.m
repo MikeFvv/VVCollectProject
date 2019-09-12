c53f407bb94448f311018115da26908b9755437c
PPPname <PPP@gmail.com>
2019-09-12 20:24:32 +0800

    d

--- VVCollectProject/Classes/Main/Baccarat/BaccaratController.m
+++ VVCollectProject/Classes/Main/Baccarat/BaccaratController.m
@@ -3,7 +3,7 @@
 //  VVCollectProject
 //
 //  Created by Mike on 2019/2/20.
-//  Copyright © 2019 Mike. All rights reserved. 1 
+//  Copyright © 2019 Mike. All rights reserved. 1 2
 //
 
 #import "BaccaratController.h"
