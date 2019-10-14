24747a5e0ba4ff2321259406556fe0a913076137
AFan <FXXXFan@gmail.com>
2019-10-14 14:07:38 +0800

    添加自动转换 中文的 类 ， 比如 json 格式字符串里面含有 UTF8字符

---
+++ VVCollectProject/Classes/Expand/Category/PrintBeautifulLog.h
@@ -0,0 +1,11 @@
+//
+//  PrintBeautifulLog.h
+//  PrintBeautifulLog
+//
+//  Created by 石学谦 on 2018/9/14.
+//  Copyright © 2018年 shixueqian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
