2c26316301fb7eec1561fd54b39ce4f8a4763beb
fhqq <fhqrq@gamil.com>
2020-02-11 18:59:45 +0800

    代码块

--- VVCollectProject/Classes/Main/CodeSnippet/MMPropertyCodingBlock.h
+++ VVCollectProject/Classes/Main/CodeSnippet/MMPropertyCodingBlock.h
@@ -9,49 +9,48 @@
 #ifndef MMPropertyCodingBlock_h
 #define MMPropertyCodingBlock_h
 
+/// <#xxx属性 #">
 
-/**
- *  const属性 xxconst
- */
+
+/// const属性 xxconst
+/// <#const属性#>
 static NSString *const <#varibleName#> = @"<#varibleValue#>";
 
-
-/**
- *  weak属性 xxweak
- */
+/// weak属性 xxweakP
+/// <#weak属性#>
 @property (weak, nonatomic) <#class#> *<#pram#>;
 
-/**
- *  strong属性 xxstrong
- */
+/// strong属性 xxstrong
+/// <#strong属性#>
 @property (strong, nonatomic) <#class#> *<#pram#>;
 
-/**
- *  assign属性 xxassign
- */
+/// assign属性 xxassign
+/// <#assign属性#>
 @property (assign, nonatomic) <#class#> <#pram#>;
 
-/**
- *  copy属性 xxcopy
- */
+/// copy属性 xxcopy
+/// <#copy属 #>
 @property (copy, nonatomic) <#class#> *<#pram#>;
 
-/**
- *  NSString属性 xxstring
- */
+/// NSString属性 xxstring
+/// <#NSString属性#>
 @property (copy, nonatomic) NSString *<#pram#>;
 
-/**
- *  block属性 xxblock
- */
+/// block属性 xxblock
+/// <#block属性#>
 @property (copy, nonatomic) <#ReturnType#>(^<#BlockName#>)(<#param#>);
 
+/// block属性 xxdelegate
 /// <#delegate注释#>
 @property (nonatomic, weak) id<<#protocol#>> <#delegate#>;
 
 
-
+/// xxmark
 #pragma mark -  <#要注释的内容#>
+
+/// xxseak
+__weak __typeof(self)weakSelf = self;
+__strong __typeof(weakSelf)strongSelf = weakSelf;
 
 
 
