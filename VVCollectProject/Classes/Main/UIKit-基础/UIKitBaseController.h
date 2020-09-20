2c8e8d2a82a953e57f7cdd15dbd45ec86bf0fc9f
ukk <ukkoop@gmail.com>
2020-09-21 01:23:16 +0800

    11

---
+++ VVCollectProject/Classes/Main/UIKit-基础/UIKitBaseController.h
@@ -0,0 +1,17 @@
+//
+//  UIKitBaseController.h
+//  VVCollectProject
+//
+//  Created by lvan Lewis on 2020/9/20.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIKitBaseController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
