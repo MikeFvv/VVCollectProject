6f1085eadbdf72a93de5795af0c271628635adf5
Mike <MikeFvv@gmail.com>
2021-12-09 09:13:49 +0800

    bai 发牌视图

--- VVCollectProject/Classes/Main(项目)/Common/PlayCardModel.h
+++ VVCollectProject/Classes/Main(项目)/Common/PlayCardModel.h
@@ -1,7 +1,14 @@
 
 
 #import <Foundation/Foundation.h>
-#import "BlackJackDataSourceModel.h"
+
+// 花色 从小到大
+typedef NS_ENUM(NSInteger, PokerColorType) {
+    PokerColorType_Diamonds = 0,   //  方块
+    PokerColorType_Clubs = 1,   // 梅花
+    PokerColorType_Hearts = 2,    // 红桃（红心）
+    PokerColorType_Spades = 3,       // 黑桃
+};
 
 
 @interface PlayCardModel : NSObject
--- VVCollectProject/Classes/Main(项目)/Common/PlayCardModel.m
+++ VVCollectProject/Classes/Main(项目)/Common/PlayCardModel.m
@@ -4,11 +4,4 @@
 
 @implementation PlayCardModel
 
-@synthesize suitSymbol = _suitSymbol,
-            suitText = _suitText,
-            cardValue = _cardValue, 
-            alterValue = _alterValue, 
-            cardText = _cardText, 
-            longName = _longName;
-
 @end
