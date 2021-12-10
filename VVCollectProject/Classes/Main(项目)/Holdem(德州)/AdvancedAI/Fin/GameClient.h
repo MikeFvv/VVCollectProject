0cee38797b35c2eb66d1ccb345ff11ffd28f9eca
Mike <MikeFvv@gmail.com>
2021-12-11 06:41:06 +0800

    23

---
+++ VVCollectProject/Classes/Main(项目)/Holdem(德州)/AdvancedAI/Fin/GameClient.h
@@ -0,0 +1,17 @@
+//
+//  GameClient.h
+//  VVCollectProject
+//
+//  Created by Admin on 2021/12/11.
+//  Copyright © 2021 Mike. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GameClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VVCollectProject/Classes/Main(项目)/Holdem(德州)/AdvancedAI/Fin/SelectAI.h
@@ -0,0 +1,20 @@
+//
+//  SelectAI.h
+//  VVCollectProject
+//
+//  Created by Admin on 2021/12/11.
+//  Copyright © 2021 Mike. All rights reserved.
+//
+
+#import "SuperAI.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SelectAI : SuperAI
+public final static int NUM_6_8 = 0;
+    public final static int NUM_4_5 = 1;
+    public final static int NUM_3 = 2;
+    public final static int NUM_2 = 3;
+@end
+
+NS_ASSUME_NONNULL_END
