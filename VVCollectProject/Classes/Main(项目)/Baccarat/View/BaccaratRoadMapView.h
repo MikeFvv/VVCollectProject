d540897c6488b775b9a827fc99755fd5615c94bd
Mike <MikeFvv@gmail.com>
2021-12-18 04:39:44 +0800

    11

--- VVCollectProject/Classes/Main(项目)/Baccarat/View/BaccaratRoadMapView.h
+++ VVCollectProject/Classes/Main(项目)/Baccarat/View/BaccaratRoadMapView.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 typedef void (^HeadClickBlock)(NSInteger index);
 
+/// *** 大路 ***
 @interface BaccaratRoadMapView : UIView
 
 @property (nonatomic, copy) HeadClickBlock headClickBlock;
