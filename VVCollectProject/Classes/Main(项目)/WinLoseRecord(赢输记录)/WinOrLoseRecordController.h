eaaf39865e4e3c47710d6a455e5541b3751823e3
Mike <MikeFvv@gmail.com>
2022-02-18 06:51:22 +0800

    赢输记录表

---
+++ VVCollectProject/Classes/Main(项目)/WinLoseRecord(赢输记录)/WinOrLoseRecordController.h
@@ -0,0 +1,17 @@
+//
+//  WinOrLoseRecordController.h
+//  VVCollectProject
+//
+//  Created by Admin on 2022/2/18.
+//  Copyright © 2022 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WinOrLoseRecordController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
